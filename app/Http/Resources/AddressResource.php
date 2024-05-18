b41a18bd921d8bc7ecf9cc6d70f1a2f15fef7c60
Mohamed Galal <mgalal8866@gmail.com>
2024-05-18 05:30:46 +0300

    modified:   app/Http/Resources/AddressResource.php

--- app/Http/Resources/AddressResource.php
+++ app/Http/Resources/AddressResource.php
@@ -12,15 +12,17 @@ class AddressResource extends JsonResource
 
     public function toArray(Request $request): array
     {
+        if ($request->has('lat') && $request->has('lat') &&  !empty($request->lat) &&  !empty($request->long)) {
+            $response = distancematrix($this->lat . ',' . $this->long, ($request->lat ?? $this->lat) . ',' . ($request->long ?? $this->long));
+            $response = $response['rows'][0]['elements'][0]['distance']['text'] . ',' . $response['rows'][0]['elements'][0]['duration']['text'];
+        }
 
 
-         $response = distancematrix($this->lat . ',' . $this->long, ($request->lat??$this->lat) . ',' .( $request->long?? $this->long));
 
-
-        return[
+        return [
             'name'        => $this->name,
             'address'     => $this->address,
-            'distance'     => $request->lat?$response['rows'][0]['elements'][0]['distance']['text'] . ',' . $response['rows'][0]['elements'][0]['duration']['text']:"",
+            'distance'    =>  $response??'',
 
 
 
