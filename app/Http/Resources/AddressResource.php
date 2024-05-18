2e62acdeb405ad641a396531e9a7f92208456a54
Mohamed Galal <mgalal8866@gmail.com>
2024-05-18 05:38:33 +0300

    modified:   app/Http/Resources/AddressResource.php

--- app/Http/Resources/AddressResource.php
+++ app/Http/Resources/AddressResource.php
@@ -14,8 +14,7 @@ class AddressResource extends JsonResource
     {
         if ($request->has('lat') && $request->has('lat') &&  $request->lat != '' &&   $request->long != '' && $this->lat != null  && $this->long != null  ) {
             $response = distancematrix($this->lat . ',' . $this->long, ($request->lat ?? $this->lat) . ',' . ($request->long ?? $this->long));
-            $response = $response ;
-            // $response = $response['rows'][0]['elements'][0]['distance']['text'] . ',' . $response['rows'][0]['elements'][0]['duration']['text'];
+            $response = $response['rows'][0]['elements'][0]['distance']['text'] . ',' . $response['rows'][0]['elements'][0]['duration']['text'];
         }
 
 
