4eb6690182be208b795477655cd8c76441ef93b7
Mohamed Galal <mgalal8866@gmail.com>
2024-07-07 03:58:00 +0300

    modified:   app/Http/Resources/CategoryResource.php

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -32,7 +32,7 @@ class CategoryResource extends JsonResource
             'time' =>  $time . ' / ' . $min,
             'time_int' =>  $result['min']??'' ,
             'km_int' =>  $result['km']??'',
-            'origin_addresses' =>  $response['destination_addresses']??'',
+            'origin_addresses' =>  $response??'',
             'destination_address' =>  $response['destination_addresses']??'',
 
         ];
