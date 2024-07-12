deaceee06eaf50290401d8108ef1d24d19610cb1
Mohamed Galal <mgalal8866@gmail.com>
2024-07-12 05:26:46 +0300

    modified:   config/broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,7 +36,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => '127.0.0.1',
+                'host' => 'localhost1',
                 'encrypted' => true,
                 'port' => '6002',
                 'scheme' => 'https',
