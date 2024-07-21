4b22b4458090d18c7176e94d3023f89211bb726b
Mohamed Galal <mgalal8866@gmail.com>
2024-07-22 02:41:09 +0300

    modified:   app/Http/Resources/DriverResource.php

--- app/Http/Resources/DriverResource.php
+++ app/Http/Resources/DriverResource.php
@@ -27,7 +27,7 @@ class DriverResource extends JsonResource
             'model'                    => $this->model->title,
             'lat'                      => $this->user->lat,
             'long'                     => $this->user->long,
-            'rating'                   =>  $this->user->rating->sum('stars')/$this->user->rating->count(),
+            'rating'                   =>  $this->user->rating? ($this->user->rating->sum('stars')/$this->user->rating->count()):'',
 
            ];
     }
