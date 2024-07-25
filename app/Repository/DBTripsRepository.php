f76ff47c4ffb4d097878a758139910eab8173bc4
Mohamed Galal <mgalal8866@gmail.com>
2024-07-26 02:37:37 +0300

    modified:   app/Repository/DBTripsRepository.php

--- app/Repository/DBTripsRepository.php
+++ app/Repository/DBTripsRepository.php
@@ -141,7 +141,7 @@ class DBTripsRepository implements TripsRepositoryinterface
     public function driver_trips($status)
     {
 
-        $trip =  $this->model->where(['driver_id' => Auth::user()->id])->where('status', '=', $status)->get();
+        $trip =  $this->model->where(['user_id' => Auth::user()->id])->where('status', '=', $status)->get();
         return Resp(TripResource::collection($trip), __('messages.success'), 200, true);
     }
     public function user_trips($status)
