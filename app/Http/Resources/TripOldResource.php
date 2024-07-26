599fddc49f9076225ce1f58439dd8bb3b5630c47
Mohamed Galal <mgalal8866@gmail.com>
2024-07-26 03:12:06 +0300

    modified:   app/Http/Resources/TripOldResource.php

--- app/Http/Resources/TripOldResource.php
+++ app/Http/Resources/TripOldResource.php
@@ -14,21 +14,26 @@ class TripOldResource extends JsonResource
 
     public function toArray(Request $request): array
     {
-        $user =     User::whereHas('credit',function($q){
-            $q->where('is_default',1);
-        })->find( $this->user->id);
+        $user =     User::whereHas('credit', function ($q) {
+            $q->where('is_default', 1);
+        })->find($this->user->id);
         return [
             'trip_id'                  => $this->id,
             'date'                      => $this->created_at->translatedFormat('d/m/y H:i A'),
             'origin_address'           => $this->origin_address,
             'destination_address'      => $this->destination_address,
             'distance'                 => $this->distance . 'k.m',
-            'min'                      => number_format($this->min,2),
-            'final_amount'             => $this->final_amount. ' ر.س ',
-            'payment_type'             => $user !=null? (count($user->credit) > 0 ? __('trans.credit') : __('trans.cash')): __('trans.cash') ,
+            'min'                      => number_format($this->min, 2),
+            'final_amount'             => $this->final_amount . ' ر.س ',
+            'payment_type'             => $user != null ? (count($user->credit) > 0 ? __('trans.credit') : __('trans.cash')) : __('trans.cash'),
             'status'                   => $this->status,
-            'driver'                   => $this->driver ? new DriverResource($this->driver):'',
+            'driver'                   => $this->driver ? [
+                'name'                     => $this->driver->user->name,
+                'driver_image'             =>  $this->driver->user->imageurl,
+                'brand'                    => $this->driver->brand->title,
+                'color'                    => $this->color,
+                ] : '',
             // 'user'                     => $this->user? new UserResource($this->user):''
-           ];
+        ];
     }
 }
